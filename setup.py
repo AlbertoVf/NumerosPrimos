62a181fc9c5689941603154143a11c6ba5ba6f42
AlbertoVf <AlbertoVf97@protonmail.com>
2019-11-26 22:10:38 +0100

    Create Numeros Primos desde 900_000_000 a 1_000_000_000
    Fichero ZIP de numeros primos y actualizacion de variables de creacion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from primos import *
 
-inicio = 655 * 1000* 1000
-final = 1000 * 1000 * 1000
+inicio = 1000 * 1000* 1000
+final = 1100 * 1000 * 1000
 incremento = 1000000
 
 calculadora(inicio,incremento)
