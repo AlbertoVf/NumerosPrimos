03440c604ce837ad2c080c064046bdef8dc1d1a5
AlbertoVf <albertovf97@protonmail.com>
2019-12-13 19:41:38 +0100

    Numeros primos desde 1_100_000_000 a 1_200_000_000

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,8 @@
 from src.calculos import continuarCalculos
 
 datos = {
-    'inicio': 1023 * 1000 * 1000,
-    'final': 1100 * 1000 * 1000,
+    'inicio': 12 * 100 * 1000 * 1000,
+    'final': 13 * 100 * 1000 * 1000,
     'incremento': 1000 * 1000
 }
 continuarCalculos(datos['final'], datos['incremento'])
