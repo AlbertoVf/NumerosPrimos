2d50ccd98edafd877412d856ef878b39c271f676
AlbertoVf <AlbertoVf97@protonmail.com>
2019-12-22 01:38:00 +0100

    Numeros Primos desde 1_800_000_000 a 2_000_000_000

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from src.calculos import continuarCalculos
 datos = {
-    'inicio': 17 * 100 * 1000 * 1000,
-    'final': 18 * 100 * 1000 * 1000,
+    'inicio': 20 * 100 * 1000 * 1000,
+    'final': 21 * 100 * 1000 * 1000,
     'incremento': 1000 * 1000
 }
 continuarCalculos(datos['final'], datos['incremento'])
